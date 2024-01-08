95c234b7f54f58d02dd33b15400a6ce16c2b9776
DeschampsJ <74jules74@gmail.com>
2024-01-08 11:00:40 +0100

    up

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
 var app = express();
 var os = require("os");
-var morgan  = require('morgan');
+
+app.set('view engine', 'jade');
 
 var port = process.env.PORT || 8080;
 var message = process.env.MESSAGE || "Learning Docker!";
